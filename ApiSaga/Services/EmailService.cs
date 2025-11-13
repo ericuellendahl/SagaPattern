203575d2f3ad109076cde109a99d6940331822ae
Eric Albert Sabino Pinho <eric.uellendahl@gmail.com>
2025-11-13 15:23:52 -0300

    Update log identifier in EmailService
    The log message identifier in the `EnviarEmailAsync` method of the
    `EmailService` class was updated from "2-3" to "3-2". This change
    affects the `logger.LogInformation` call, which logs the action of
    sending an email. The update is intended for improved tracking or
    categorization of log entries. No functional behavior of the method
    was altered.

--- ApiSaga/Services/EmailService.cs
+++ ApiSaga/Services/EmailService.cs
@@ -9,7 +9,7 @@ public class EmailService(ILogger<EmailService> logger) : IEmailService
 {
     public Task EnviarEmailAsync(string destinatario, string assunto, string corpo)
     {
-        logger.LogInformation("2-3 Enviando email para {Destinatario} com assunto '{Assunto}' e corpo '{Corpo}'",
+        logger.LogInformation("3-2 Enviando email para {Destinatario} com assunto '{Assunto}' e corpo '{Corpo}'",
             destinatario, assunto, corpo);
 
         return Task.CompletedTask;
